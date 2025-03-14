bd3d88b05b1d2934da6111794adac077b5d7eae2
reidwatson <davisreidwatson@gmail.com>
2025-03-14 11:13:49 -0500

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -18,5 +20,6 @@ export default defineConfig({
         secure: false
       },
     },
+    allowedHosts: ['reidsjobboard.com', 'www.reidsjobboard.com'],
   },
 }) 
